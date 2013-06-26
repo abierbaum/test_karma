dfb832baabbc909437d05c2b55fe685ac105dd0f
Allen Bierbaum <abierbaum@gmail.com>
2013-06-26 09:31:06 -0500

    Update to canary karma.

--- karma.conf.js
+++ karma.conf.js
@@ -1,75 +1,94 @@
 // Karma configuration
-// Generated on Tue Jun 25 2013 13:16:00 GMT-0500 (CDT)
+// Generated on Wed Jun 26 2013 09:06:15 GMT-0500 (CDT)
+
+module.exports = function(karma) {
+  karma.configure({
+
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
+
+    plugins: [
+      'karma-jasmine',
+      'karma-chrome-launcher',
+      'karma-phantomjs-launcher',
+      'karma-coverage'
+    ],
+
+    // frameworks to use
+    frameworks: ['jasmine'],
 
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
-
-//frameworks = ['jasmine'];
-
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'src/*.js',
-  'spec/*.js'
-];
+    // list of files / patterns to load in the browser
+    files: [
+      'src/*.js',
+      'spec/*.js'
+    ],
 
 
-// list of files to exclude
-exclude = [
-  
-];
+    // list of files to exclude
+    exclude: [
+      
+    ],
 
-preprocessors = {
-  'src/*.js': 'coverage'
+    preprocessors: {
+      'src/*.js': 'coverage'
+    },
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress', 'coverage'],
+
+    //Code Coverage options. report type available:
+    //- html (default)
+    //- lcov (lcov and html)
+    //- lcovonly
+    //- text (standard output)
+    //- text-summary (standard output)
+    //- cobertura (xml format supported by Jenkins)
+    coverageReporter: {
+        // cf. http://gotwarlost.github.io/istanbul/public/apidocs/
+        type : 'text-summary',
+        dir : 'coverage/'
+    },
+
+    // web server port
+    port: 9876,
+
+
+    // cli runner port
+    runnerPort: 9100,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: karma.LOG_DISABLE || karma.LOG_ERROR || karma.LOG_WARN || karma.LOG_INFO || karma.LOG_DEBUG
+    logLevel: karma.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['Chrome'],
+
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+  });
 };
-
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-reporters = ['progress','coverage'];
-
-coverageReporter = {
-  type: 'text-summary',
-  dir : 'coverage/'
-};
-
-// web server port
-port = 9876;
-
-
-// cli runner port
-runnerPort = 9100;
-
-
-// enable / disable colors in the output (reporters and logs)
-colors = true;
-
-
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
-
-
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
-
-
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
-
-
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 60000;
-
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
